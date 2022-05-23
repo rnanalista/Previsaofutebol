5884527d100d619b44221f4deb34bc22cc07cb2b
Renan Gonçalves Silva <pontualtrabalho@hotmail.com>
2022-05-22 21:58:30 -0400

    Update futebol_classificar.R

--- futebol_classificar.R
+++ futebol_classificar.R
@@ -1,5 +1,5 @@
 #################################################################################
-# Este mini projeto tem como objetivo criar um modelo supervisionado que consiga    #
+# Este mini projeto tem como objetivo criar um modelo supervisionado que consiga#
 # prever se um time vai ganhar, empatar ou perde o próximo jogo com base nas    #
 # estáticas das últimas partidas dele.                                          #
 #################################################################################
